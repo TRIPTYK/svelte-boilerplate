b8a4b1de2a9f399ce6ffa62b8d87ea597bfc6b8b
remadex <s.cardon@outlook.com>
2023-06-12 17:34:54 +0200

    update readme

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
+// @ts-ignore
 import App from './App.svelte';
 import { create_custom_element } from 'svelte/internal';
 
 
 customElements.define('my-app',
+	// @ts-ignore
 	create_custom_element(
 		App,
 		{},
