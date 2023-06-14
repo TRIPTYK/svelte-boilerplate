6106c95eddb816a18beaf2241f8c2b3204d0303b
remadex <s.cardon@outlook.com>
2023-06-14 21:22:42 +0200

    add scope css with adding a surrouding class

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,21 @@
 import tailwind from 'tailwindcss'
 import tailwindConfig from './tailwind.config.js'
 import autoprefixer from 'autoprefixer'
+import prefixer from 'postcss-prefix-selector';
 
 export default {
-  plugins:[tailwind(tailwindConfig),autoprefixer]
+  plugins:[
+    tailwind(tailwindConfig),
+    autoprefixer,
+    prefixer({
+      prefix: 'my-app',
+      transform(prefix, selector) {
+        if (selector.match(/^()/)) {
+           return selector.replace(/^([^\s]*)/, ` ${prefix} $1`);
+        }
+  
+        return selector;
+      },
+    })
+  ]
 }
--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,5 @@ export default defineConfig({
         format: 'iife',
       },
     }
-  }
+  },
 })
